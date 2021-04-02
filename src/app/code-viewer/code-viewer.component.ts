eea6856d7e63e5483d522eb8facfdeef6f303652
michael <michael.forgacs@verint.com>
2021-04-02 21:48:00 +0300

    dark background color for code editor

--- src/app/code-viewer/code-viewer.component.ts
+++ src/app/code-viewer/code-viewer.component.ts
@@ -1,16 +1,16 @@
-import {Component, EventEmitter, HostListener, Input, OnInit, Output, ViewChild} from '@angular/core';
-import {AppComponent, CurrentFile} from '../app.component';
-import {AceEditorComponent} from 'ng2-ace-editor';
-import {Ace} from 'ace-builds';
-import {Utils} from '../chart/Utils';
-import {AttributesKey, ChartUtils} from '../chart/chart.utils';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { AppComponent, CurrentFile } from '../app.component';
+import { AceEditorComponent } from 'ng2-ace-editor';
+import { Ace } from 'ace-builds';
+import { Utils } from '../chart/Utils';
+import { AttributesKey, ChartUtils } from '../chart/chart.utils';
 
 export interface AceSelectionRange {
   start: { row, column },
   end: { row, column }
 }
 
-declare var ace:any;
+declare var ace: any;
 var Range = ace.require('ace/range').Range
 
 @Component({
@@ -23,18 +23,17 @@ export class CodeViewerComponent implements OnInit {
   @HostListener("keyup", ["$event"])
   @HostListener("keydown", ["$event"])
   @HostListener("keypress", ["$event"])
-  public onClick(event: any): void
-  {
-      event.stopPropagation();
+  public onClick(event: any): void {
+    event.stopPropagation();
   }
 
   @Input() showTopBar: boolean = true;
   _fileData: CurrentFile = null;
-  public fileInfo: {folder, file} = null
+  public fileInfo: { folder, file } = null
   @Input() set fileData(fileData: CurrentFile) {
-    if(!fileData) {this._fileData = null; return}
+    if (!fileData) { this._fileData = null; return }
     this._fileData = Utils.deepCopy(fileData);
-    setTimeout(()=>{this.setMode()}, 200);
+    setTimeout(() => { this.setMode() }, 200);
     this.fileInfo = {
       folder: this._fileData.name ? this._fileData.name.replace(/^.*[\\\/]/, '') : '',
       file: this._fileData.name ? this._fileData.name.replace(/\w+\..*/, '') : ''
@@ -71,7 +70,7 @@ export class CodeViewerComponent implements OnInit {
   }
 
   setMode() {
-    if(!(this.fileData && this.fileData.name && this.fileData.name.split('.').length)) return
+    if (!(this.fileData && this.fileData.name && this.fileData.name.split('.').length)) return
     let split = this.fileData.name.split('.');
     if (split.length === 1) return;
     let suffix = split[split.length - 1];
@@ -112,20 +111,20 @@ export class CodeViewerComponent implements OnInit {
       case 'ino':
         this.editor.setMode('c_cpp');
         break;
-      default:         this.editor.setMode('c_cpp');
+      default: this.editor.setMode('c_cpp');
 
     }
   }
 
   ngOnInit() {
 
-    this.editor.setTheme('chrome');
+    // this.editor.setTheme("ace/theme/twilight");
     this.aceEditor = this.editor.getEditor();
     this.aceEditor.setAnimatedScroll(true);
-    this.aceEditor.getSelection().on('changeCursor', (a,b,c) => {
-      let selection  = this.aceEditor.getSelection()
+    this.aceEditor.getSelection().on('changeCursor', (a, b, c) => {
+      let selection = this.aceEditor.getSelection()
       console.log(this.aceEditor.getSelectedText())
-      if(selection.getAnchor().row == 0 && selection.getAnchor().column == 0) return
+      if (selection.getAnchor().row == 0 && selection.getAnchor().column == 0) return
       this.selectionChange.emit(this.aceEditor.getSelection());
     });
 
@@ -133,19 +132,19 @@ export class CodeViewerComponent implements OnInit {
     this.setMode();
     this.aceEditor.setOption('foldStyle', 'markbeginend');
     this.aceEditor.setOption('scrollPastEnd', true);
-    this.aceEditor.on('blur', (event)=>{this.blurEvent(event)})
-    this.aceEditor.on('focus', (event)=>{this.focusEvent(event)})
-    this.aceEditor.on("change", (event)=>{this.changeText(event)})
+    this.aceEditor.on('blur', (event) => { this.blurEvent(event) })
+    this.aceEditor.on('focus', (event) => { this.focusEvent(event) })
+    this.aceEditor.on("change", (event) => { this.changeText(event) })
   }
 
   changeText(event) {
     try {
-      if(!(this.appComponent && this.appComponent.currentFile && this.appComponent.currentFile.node)) {
+      if (!(this.appComponent && this.appComponent.currentFile && this.appComponent.currentFile.node)) {
         console.log('no file selectd')
         return
       }
       let fileNode = this.appComponent.currentFile.node
-      if(ChartUtils.isCustomNode(fileNode)) {
+      if (ChartUtils.isCustomNode(fileNode)) {
         ChartUtils.setFileContent(fileNode, this.aceEditor.session.getValue(), this.appComponent.chart)
       }
     } catch (ex) {
@@ -162,7 +161,7 @@ export class CodeViewerComponent implements OnInit {
     this.changeFileContentFonSize(-5);
   }
 
- 
+
 
   changeFileContentFonSize(howMuch: number) {
     this.fontSize = this.fontSize + howMuch;
@@ -185,7 +184,7 @@ export class CodeViewerComponent implements OnInit {
 
   public markLinesSelected(startRowNumber, endRowNumber) {
 
-    let range = new Range(0,0,0,0)
+    let range = new Range(0, 0, 0, 0)
     this.setRangeForStartEndLines(range, startRowNumber, endRowNumber);
     if (this.lastAddedMarker) {
       this.aceEditor.getSession().removeMarker(this.lastAddedMarker);
@@ -194,7 +193,7 @@ export class CodeViewerComponent implements OnInit {
   }
 
   setRangeForStartEndLines(range: Ace.Range, startRowNumber, endRowNumber): Ace.Range {
-    if(!startRowNumber) {
+    if (!startRowNumber) {
       console.log('no start line number')
       return
     }
@@ -209,12 +208,12 @@ export class CodeViewerComponent implements OnInit {
 
   public markMatchesInFile(matches: { startRowNumber, endRowNumber }[]) {
     var Range = ace.require('ace/range').Range
-    this.matchMarkers.forEach(i=>{
+    this.matchMarkers.forEach(i => {
       this.aceEditor.getSession().removeMarker(i)
     })
     this.matchMarkers = []
-    matches.forEach(i=>{
-      let range = new Range(0,0,0,0)
+    matches.forEach(i => {
+      let range = new Range(0, 0, 0, 0)
       this.setRangeForStartEndLines(range, i.startRowNumber, i.endRowNumber)
       let addedMarker = this.aceEditor.getSession().addMarker(range, 'matchMarker', 'fullLine');
       this.matchMarkers.push(addedMarker)
@@ -222,6 +221,6 @@ export class CodeViewerComponent implements OnInit {
   }
 
   saveFile() {
-    this.appComponent.saveLoad.saveToCode([{name: this.appComponent.currentFile.name, content: this.aceEditor.session.getValue()}])
+    this.appComponent.saveLoad.saveToCode([{ name: this.appComponent.currentFile.name, content: this.aceEditor.session.getValue() }])
   }
 }
